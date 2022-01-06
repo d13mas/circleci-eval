dc5cca46bc620021be64c9eb565d6130f144911d
Diego Maslo <diego@corewpress.com>
2022-01-06 18:37:47 -0500

    Now build

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Esto esta roto
+          learn react
         </a>
       </header>
     </div>
